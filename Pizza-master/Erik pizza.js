bacbe9c980a61ee42d2b42e209fd01125c9da096
ErikJachym <7ejachym@stpatrick.org>
2016-11-17 13:57:47 -0600

    Add files via upload
    DID Stuff

---
+++ Pizza-master/Erik pizza.js
@@ -0,0 +1,44 @@
+class pizza{
+  //type constructor below
+
+
+constructor (s,mT,vT){
+  this.size = s;
+  this.meatToppings = mT;
+  this.veggieToppings = vT;
+}
+  //type instance functions below
+
+sizecost(){
+  if (this.size = "S"){
+    return 7.99;
+  }
+  else if (this.size = "M"){
+    return 9.99;
+  }
+   else if (this.size = "L"){
+  return 12.99;
+  }
+   else if (this.size = "XL"){
+  return 15.99;
+}
+}
+
+toppingCost(top){
+  if (this.top == "meat"{
+  return .99*this.meatToppings.length;)
+}
+else if (this.top == "veggie"{
+return .50*this.veggieToppings.length;)
+}
+}
+
+cost(){
+  return (this.sizeCost()+this.meatToppingsCost()+this.veggieToppingsCost();
+}
+  //type class functions below
+
+static promotionalDeal(){
+
+}
+}
